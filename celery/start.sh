9ed50c2cfc01236bb9dd91e85c44b8afb1aefb77
Alex Radnaev <alexander.radnaev@gmail.com>
2025-08-19 19:33:08 -0600

    updated celery start config; added celery memory usage logging

--- celery/start.sh
+++ celery/start.sh
@@ -6,4 +6,11 @@ echo Starting Celery.
 cd $PROJECT_ROOT
 cd etabotsite
 
-celery -A etabotsite worker -l info --max-tasks-per-child=4
+celery -A etabotsite worker \
+  -l info \
+  --max-tasks-per-child=4 \
+  --concurrency=2 \
+  --prefetch-multiplier=1 \
+  --without-gossip \
+  --without-mingle \
+  --without-heartbeat
