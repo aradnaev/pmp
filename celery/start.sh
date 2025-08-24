59de0effd8f67e026c8fed6c80fbf6fc925752d9
Alex Radnaev <alexander.radnaev@gmail.com>
2025-08-24 12:34:21 -0600

    added graceful shut down of container and logging

--- celery/start.sh
+++ celery/start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+log_msg() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [START.SH] $1"
+}
+
 echo Starting Celery.
 
 #make sure we are at project root /usr/src/app
 cd $PROJECT_ROOT
 cd etabotsite
+
+trap 'echo "Shutting down gracefully..."; kill -TERM $celery_pid; wait $celery_pid' SIGTERM SIGINT
 
 celery -A etabotsite worker \
   -l info \
@@ -14,3 +20,19 @@ celery -A etabotsite worker \
   --without-gossip \
   --without-mingle \
   --without-heartbeat
+
+celery_pid=$!
+log_msg "Celery worker started with PID: $celery_pid"
+
+wait $celery_pid
+
+exit_code=$?
+
+log_msg "=== CELERY WORKER EXITED ==="
+log_msg "Celery worker exit code: $exit_code"
+
+if [ $exit_code -eq 0 ]; then
+    log_msg "Celery worker exited normally"
+else
+    log_msg "Celery worker exited with error code: $exit_code"
+fi
