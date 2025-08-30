6524e80fa44b264e26e802f2e89965e920ca83ed
Alex Radnaev <alexander.radnaev@gmail.com>
2025-08-30 17:51:43 -0600

    workaround for missing start field property

--- etabotsite/etabotapp/django_tasks.py
+++ etabotsite/etabotapp/django_tasks.py
@@ -115,7 +115,11 @@ def generate_critical_path_jira(
         try:
             issue = create_jira_issue_from_json(issue_dict)
             tasks.append(issue)
-            start_date = issue.get_field(start_date_field_name, None)
+            try:
+                start_date = issue.get_field(start_date_field_name)
+            except Exception as e:
+                logger.warning(f'cannot get start_date for issue {issue.key} from {start_date_field_name} due to {e}')
+                start_date = None
             if start_date is None:
                 logger.warning(f'start_date is None for issue {issue.key}.'
                                f'dict {start_date_field_name}: {issue_dict["fields"].get(start_date_field_name)}.'
